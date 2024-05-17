0f92f0fd40a225198b56a9bdb54373421c5706f4
João Paulo <joao.rochap03@gmail.com>
2024-05-17 17:17:00 -0300

    Mudando background de menu e iniciando home

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,19 @@ function activeMenu(){
     const body = document.querySelector("body");
     body.classList.toggle("menu-on");
 }
+
+
+const aHeader = document.querySelectorAll(".navigation-header a");
+
+for(var i = 0; i < aHeader.length; i++){
+    aHeader[i].onclick = () => {
+        body.classList.remove("menu-on");
+    }
+}
+
+const header = document.querySelector("header");
+function activeHeader(){
+    header.classList.toggle("activeHeader", scrollY > 0);
+}
+
+document.addEventListener("scroll", activeHeader);
